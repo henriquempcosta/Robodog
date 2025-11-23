4593f13c4e0622135e985af8e9f0e42fccd66f2f
Henrique <henriquempcosta@gmail.com>
2025-11-23 14:19:48 +0000

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -7,8 +7,9 @@ for (let index = 0; index < 2; index++) {
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
 }
-basic.pause(10000)
+basic.pause(6700)
 maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
 basic.pause(2000)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-basic.pause(50000)
+basic.pause(5000)
+maqueen.motorStop(maqueen.Motors.All)
